345a85369be35f267fd8061af465b47b7b6c7714
Will Nicholson <william.nicholson.dev@gmail.com>
2020-12-14 17:44:42 -0600

    final commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 
 window.addEventListener("load", function(){
+   
    //Get form data
-    
    let formSubmit = document.getElementById("formSubmit");
       let pilotName = document.getElementById("pilotName");
       let copilotName = document.getElementById("copilotName");
@@ -18,19 +18,20 @@ window.addEventListener("load", function(){
    let launchStatus = document.getElementById("launchStatus");
    let missionTarget = document.getElementById("missionTarget");
 
-   //fetch
+   //fetch planet at random
 fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(res){
    res.json().then(function(json){
+      const rand = Math.round(Math.random() * (json.length - 1));
       missionTarget.innerHTML = `
         <h2>Mission Destination</h2>
         <ol>
-           <li>Name: ${json[2].name}</li>
-           <li>Diameter: ${json[2].diameter}</li>
-           <li>Star: ${json[2].star}</li>
-           <li>Distance from Earth: ${json[2].distance}</li>
-           <li>Number of Moons: ${json[2].moons}</li>
+           <li>Name: ${json[rand].name}</li>
+           <li>Diameter: ${json[rand].diameter}</li>
+           <li>Star: ${json[rand].star}</li>
+           <li>Distance from Earth: ${json[rand].distance}</li>
+           <li>Number of Moons: ${json[rand].moons}</li>
         </ol>
-        <img src="${json[2].image}"></img>
+        <img src="${json[rand].image}"></img>
         ` 
       })
    })  
@@ -46,21 +47,21 @@ fetch("https://handlers.education.launchcode.org/static/planets.json").then(func
             pilotStatus.innerText = `Pilot: ${pilotName.value} ready for launch.`
          }
       if(pilotName.value === ""){
-         window.alert("All fields required.")
+         window.alert("All fields are required.")
          event.preventDefault();
          }else{
                copilotStatus.innerText = `Co-pilot: ${copilotName.value} ready for launch.`
          }   
    //If cargoMass or fuelLevel are not numbers, alert.
       if(isNaN(cargoMass.value) || isNaN(fuelLevel.value) || fuelLevel.value === "" || cargoMass.value === "" ){
-         window.alert("Fuel Level and Cargo Mass must be a valid number.")
+         window.alert("Make sure to enter valid information for each field!")
          event.preventDefault();
          }else{
             checkValues();
          }
 
       })
-   
+
 function checkValues(){
       if(fuelLevel.value < 10000){
          fuelStatus.innerText = "Not enough fuel for journey."
